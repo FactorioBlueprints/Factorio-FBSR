7ebf65e5f43651393a7cb639e004d9ae72f626f5
Factorio Blueprints <factorio.prints@gmail.com>
2026-07-26 17:51:11 +0000

    Use character crafting categories for total raw calculations.

--- FactorioBlueprintStringRenderer/src/com/demod/fbsr/FBSR.java
+++ FactorioBlueprintStringRenderer/src/com/demod/fbsr/FBSR.java
@@ -698,7 +698,7 @@ public class FBSR {
 		DataTable baseTable = factorioManager.getProfileVanilla().getFactorioData().getTable();
 		Map<String, RecipePrototype> recipes = baseTable.getRecipes();
 		Map<BSItemWithQualityID, Double> ret = new LinkedHashMap<>();
-		TotalRawCalculator calculator = new TotalRawCalculator(recipes);
+		TotalRawCalculator calculator = new TotalRawCalculator(recipes, baseTable.getCharacterCraftingCategories());
 		for (Entry<BSItemWithQualityID, Double> entry : totalItems.entrySet()) {
 			BSItemWithQualityID recipeItem = entry.getKey();
 			double recipeAmount = entry.getValue();
